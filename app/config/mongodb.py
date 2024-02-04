1875463e9f60f41416948503e6718a49ecc82f18
LAPTOP-0RB3OV43\ASUS <pmhien2703@gmail.com>
2024-02-04 11:11:16 +0700

    Modify mongodb add try catch when establishing connect to mongo cloud

--- app/config/mongodb.py
+++ app/config/mongodb.py
@@ -3,18 +3,13 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-# replace the admin and <password> you have create before connect
 uri = os.getenv("MONGODB_CONNECT_STRING") 
-print(uri)
-uri = "mongodb+srv://admin:admin123@cluster0.sizb9bz.mongodb.net/?retryWrites=true&w=majority"
-
-# There are 2 way to connect mongodb. 
-# pymongo for synchronous Python applications.
-# motor for asynchronous Python applications
 
 # Create a new client and connect to the server
-
 def get_db():
     client = AsyncIOMotorClient(uri)
     db = client["kit"]
-    return db
+    try:
+        yield db
+    finally:
+        client.close()
