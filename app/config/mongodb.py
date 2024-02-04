e6fa0d1a67c6ea1316c0dfe5c85c1fe0db836461
LAPTOP-0RB3OV43\ASUS <pmhien2703@gmail.com>
2024-02-04 11:04:21 +0700

    feat: add instance and remove unused fields
    add get_db function to get dbcontext instance
    delete and move collection into each repository

--- app/config/mongodb.py
+++ app/config/mongodb.py
@@ -1,5 +1,3 @@
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
 import os
@@ -15,20 +13,8 @@ uri = "mongodb+srv://admin:admin123@cluster0.sizb9bz.mongodb.net/?retryWrites=tr
 # motor for asynchronous Python applications
 
 # Create a new client and connect to the server
-# client = MongoClient(uri, server_api=ServerApi('1'))
-client = AsyncIOMotorClient(uri)
 
-# Create DB
-db = client["kit"]
-# db = client.kit
-users_collection = db["users"]
-conversations_collection = db["conversations"]
-messages_collection = db["messages"]
-
-def mongo_test_connection(): 
-    # Send a ping to confirm a successful connection
-    try:
-        client.admin.command('ping')
-        print("Pinged your deployment. You successfully connected to MongoDB!")
-    except Exception as e:
-        print(e)
+def get_db():
+    client = AsyncIOMotorClient(uri)
+    db = client["kit"]
+    return db
