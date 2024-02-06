b197ae116af7bebb719dc6d1631d6e0c4a7ffb55
LAPTOP-0RB3OV43\ASUS <pmhien2703@gmail.com>
2024-02-06 22:53:49 +0700

    fix: missing 1 Validation error for Settings

--- app/config/mongodb.py
+++ app/config/mongodb.py
@@ -1,8 +1,9 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+
 from app.config.settings import Settings
 
-settings=Settings()
-uri=settings.MONGODB_CONNECT_STRING
+settings = Settings()
+uri = settings.MONGODB_CONNECT_STRING
 
 # Create a new client and connect to the server
 def get_db():
--- app/config/settings.py
+++ app/config/settings.py
@@ -3,4 +3,4 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     MONGODB_CONNECT_STRING: str
     class Config:
-        env_file = './.env'
+        env_file = './app/.env'
